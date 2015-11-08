d28c4d34f32836c4c042a8d40d9973be86a59d87
kthotav <kthotav@gmail.com>
2015-11-08 12:41:36 -0800

    clean data, ready to be inserted into pv table

--- Final_Project/pv_data/clean.py
+++ Final_Project/pv_data/clean.py
@@ -1,8 +1,7 @@
 # File clean.py
 # Cleans all the csv files in the pv_data folder
 
-import glob
-
+import glob, os, csv
 
 def cleanCSVFile(csv_file):
     infile = open(csv_file, 'r')
@@ -24,7 +23,30 @@ def cleanCSVFile(csv_file):
     infile.close()
     outfile.close()
 
+def removeRows(csv_file):
+    remove_from = 1
+    remove_to = 12
+
+    with open(csv_file, "r") as fp_in, open("final"+csv_file, "w+") as fp_out:
+        fp_in.next()
+        reader = csv.reader(fp_in)
+        writer = csv.writer(fp_out)
+        for row in reader:
+            del row[remove_from:remove_to]
+            del row[2]
+            del row[2]
+            del row[4]
+            writer.writerow(row)
+
 
 for csv_file in glob.glob("*.csv"):
     if "clean" not in csv_file:
         cleanCSVFile(csv_file)
+
+
+for csv_file in glob.glob("*.csv"):
+    if "clean" in csv_file:
+        removeRows(csv_file)
+
+for hgx in glob.glob("clean*.csv"):
+    os.remove(hgx)
