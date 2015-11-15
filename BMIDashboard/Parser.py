5b4bad5c87014681fddee0835ad88bad89e61a0a
jbarros23 <jbarros@ucsc.edu>
2015-11-15 15:00:39 -0800

    commented files

--- BMIDashboard/Parser.py
+++ BMIDashboard/Parser.py
@@ -1,19 +1,35 @@
 import glob
 import os
 
+# Opens a file, reads it's contents, and returns it as a single string variable.
+# called "data".
 def openFile(filepath):
     f = open(filepath, "r")
     data = f.read()
     return data
 
+# Cleans the specified file located at filepath. For ESO files.
+# data:         Each line of the file located at file path.
+# dname:        The entire directory path of the file, exluding the actual file name.
+# bname:        The basename of the directory path, which is the file name.
+# newbasepath:  The new basename that we will be putting the clean files into.
+# clean_file:   The file name of the clean file.
 def cleanESOFile(filepath):
     data = openFile(filepath).splitlines()
     dname = "CLEAN_ESO\\"
     bname = os.path.basename(filepath)
     newbasepath = bname[:len(bname)-4] + "_FULL.csv"
-    clean_file = open(dname + newbasepath, "w+")
-    for d in data: clean_file.write(d + "\n")
+    clean_file = open(dname + newbasepath, "w+") # w+ is write and read only
+    for d in data:
+        clean_file.write(d + "\n")
 
+# Cleans the specified file located at filepath. For PV files.
+# data:         Each line of the file located at file path.
+# keys:         The key attributes for the PV files.
+# values:       The data values for each of the key attributes.
+# dname:        The entire directory path of the file, exluding the actual file name.
+# bname:        The basename of the directory path, which is the file name.
+# clean_file:   The file name of the clean file.
 def cleanPVFile(filepath):
     data = openFile(filepath).replace(";", ",").splitlines()
     keys = data[4]
@@ -22,32 +38,72 @@ def cleanPVFile(filepath):
     bname = os.path.basename(filepath)
     clean_file = open(dname + bname, "w+")
     clean_file.write(keys + "\n")
-    for v in values: clean_file.write(v + "\n")
+    for v in values:
+        clean_file.write(v + "\n")
 
+# Parses just the ESO file keys within the data dictionary as well as the data values correlated with each key.
+# Also it converts the ESO files into CSV files.
+# data:         Each line of the file located at file path.
+# dname:        The entire directory path of the file, exluding the actual file name.
+# bname:        The basename of the directory path, which is the file name.
+# ts_filepath:  The file path for the timestamp file.
+# ts_file:      The time stamp file. (Contains all of the timestamp values and only those values).
+# timestamps:   Reopens the time stamp file so that it can be read as a reference to indicate the latest time stamp.
+# attributes:   The attributes for each data value.
+# key:          The key that for each line in the data dictionary/
 def parseESOKeys(filepath, tsc):
     data = openFile(filepath).splitlines()
     dname = "CLEAN_ESO\\"
     bname = os.path.basename(filepath)
     
+    # Time Stamp File
     ts_filepath = "2" + bname[:len(bname)-4] + ".csv"
     ts_file = open(dname + ts_filepath, "a")
+    
+    # Inserts all of the time stamps into a file for reference so that it can be appeneded to the data values for
+    # each attribute.
     for d in data:
-        if d.startswith("2,", 0, 2): ts_file.write(d + "\n")
+        if d.startswith("2,", 0, 2):
+            ts_file.write(d + "\n")
+    # Close time stamp file and reopen it as read only.
     ts_file.close()
     timestamps = openFile(dname + ts_filepath).splitlines()
-    
+
+    # Iterate through all of the attributes within the data dictionary and create files for them if they have not
+    # already been created (d1).
     for d1 in data:
-        if d1 == "End of Data Dictionary": break
-        key = ""
-        for char in d1:
-            if char == ',': break
-            else: key += char
-        t = key + "_" + bname[:len(bname)-4] + ".csv"
-        temp_file = open(dname + t, "a")
+        if d1 == "End of Data Dictionary":
+            break
+        attributes = d1.split(",")
+        key = attributes[0]
+        t = key + "_" + bname[:len(bname)-4] + ".csv" # Create a filename for this specific key.
+        temp_file = open(dname + t, "a") # Create a file using the t variable.
+
+        # Re-iterate through the entire file to append each of the current time stamp's data values into their
+        # respective keys.
         for d2 in data:
             if d2.startswith(key + ",", 0, len(key)+1) and not d2.startswith("2,", 0, 2):
                 temp_file.write(timestamps[tsc] + "," + d2 + "\n")
 
+# Creates a data dictionary that maps each of the numerical keys to their string locations.
+def createDataDict(filepath):
+    data = openFile(filepath).splitlines()
+    dname = "CLEAN_ESO\\"
+    bname = "Data_Dictionary.csv"
+    dict_file = open(dname + bname, "w")
+    x = 0
+    for d in data:
+        if d == "End of Data Dictionary":
+            break
+        attributes = d.split(",")
+        if len(attributes) >= 4 and x >= 6:
+            val = attributes[0] + "," + attributes[2] + "," + attributes[3] + "\n"
+            dict_file.write(val)
+        x += 1
+    dict_file.close()
+    print("Dictionary has been created in directory CLEAN_ESO as 'Data_Dictionary.csv'\n")
+
+# Runs the parsing functions above depending on what type of file it is (PV or ESO).
 def parse(tsc):
     """
     print("Cleaning Raw PV Files")
@@ -56,7 +112,8 @@ def parse(tsc):
         if "CLEAN" not in filepath: cleanPVFile(filepath)
     print("PV Files Cleaned\n")
     """
-    
+
+    # Iterates through all of the files in the RAW_ESO directory and calles cleanESOFile() and parseESOKeys().
     print("Cleaning Raw ESO Files")
     for filepath in glob.glob("RAW_ESO\*.eso"):
         print(filepath)
--- BMIDashboard/Simulator.py
+++ BMIDashboard/Simulator.py
@@ -1,3 +1,8 @@
+# Simulator.py is the program that calls the mock sensor (Sensor.py) and the parser (Parser.py).
+# It threads a process to run main until the sensor has finished grabbing data from the current file that it
+# is looking in. It also keeps track of global variables that are essential to the sensor and parser programs
+# such as timestep counter (tsc) and file line number (linenr).
+
 import threading
 import sys
 import os
@@ -6,21 +11,53 @@ import Sensor
 import Parser
 
 def main():
-   global x
+   # global variables for the other modules to use as a resource.
+   #
+   # numOfAcq:    The number of data acquisitions that the program has completed for the current day.
+   #
+   # linenr:      The line number that the sensor program is currently looking at in order to simulate
+   #              the mock live data.
+   #
+   # tsc:         stands for Timestep Counter and represents the most recent timestep that the parser has
+   #              parsed thus far.
+   #
+   global numOfAcq
    global linenr
    global tsc
+
+   # tup:         A tuple pair that receives the current timestamp counter and line number that the sensor
+   #              program takes in and updates.
    tup = []
    tup = Sensor.mock(tsc, linenr)
+   
+   # If the timestamp counter >= 2 then decrement it by 1 to compensate for the offset that occurs during the
+   # identification of the first timestep line, which is 
+   # "2,6,Day of Simulation[],Month[],Day of Month[],DST Indicator[1=yes 0=no],Hour[],StartMinute[],EndMinute[],DayType"
+   # This is because that line is not a REAL timestep value. It is mere dictionary for the timestamp attributes
+   # that are going to come later on in the file.
    if tsc >= 2: tsc -= 1
+
+   # After the sensor mocks the live data by writing to a specific file, the parser goes to the file and parses it.
    Parser.parse(tsc)
-   x += 1
+   # Increment the numOfAcq variable since data has been acquired.
+   # Update the line number using the value inside tup[0] as well as the timestep counter from tup[1]
+   numOfAcq += 1
    linenr = tup[0]
    tsc = tup[1]
-   print("DATA HAS BEEN ACQUIRED " + str(x) + " TIMES\n")
-   if not (linenr == -1): threading.Timer(0, main).start()
+   print("DATA HAS BEEN ACQUIRED " + str(numOfAcq) + " TIMES\n")
+   # If the line number is -1, that means the sensor has finished reading and mocking all of the values within the
+   # current file that it is looking at. In this case, we halt the program. Otherwise, we thread a new process to
+   # execute main once again, in a recursive-like manner.
+   if not (linenr == -1): threading.Timer(0, main).start() # threads in seconds
    else: return
 
-x = 0
+# Initialize global variables
+numOfAcq = 0
 linenr = 0
 tsc = 0
+# Call parser createDataDict() to create the data dictionary for the database to use in order to map the
+# keys (e.g. 6, 7, 10, 1998, etc) to their actual values (e.g. 1STFLOOR:STAIRS, 1STFLOOR:TRASHROOM, etc).
+Parser.createDataDict("SENSOR\\MBNMS.eso")
+
+# Call main to simulate the entire mock data acquisition pipeline.
 main()
